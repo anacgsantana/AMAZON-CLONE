e5ab31d38396692700466c1a9d626d35f693c3c1
anacgsantana <anacgsantana@gmail.com>
2021-09-18 22:41:10 -0300

    first commit

--- src/index.js
+++ src/index.js
@@ -3,40 +3,52 @@ import ReactDOM from "react-dom";
 
 //CSS
 
-import './index.css';
+import "./index.css";
+const firstBook = {
+  img: "https://images-na.ssl-images-amazon.com/images/I/81eB%2B7%2BCkUL._AC_UL200_SR200,200_.jpg",
+  title: "I Love You to the moon and Back",
+  author: "Amelia Hepworth",
+};
+
+const secondBook = {
+  img: "https://m.media-amazon.com/images/I/71e38cCzk2L._AC_UY218_.jpg",
+  title: "More than a Princess",
+  author: "Delanda Coleman",
+};
 
 function BookList() {
   return (
-    <section className='booklist'>
-      <Book />
-      <Book />
-      <Book />
-      <Book />
-      <Book />
-      <Book />
-      <Book />
-      <Book />
+    <section className="booklist">
+      <Book
+        img={firstBook.img}
+        title={firstBook.title}
+        author={firstBook.author}
+      >
+        {/* adicionando children */}
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime soluta
+          ullam, nihil dolores voluptatum nulla a quo voluptates aliquam itaque?
+        </p>
+      </Book>
+      <Book
+        img={secondBook.img}
+        title={secondBook.title}
+        author={secondBook.author}
+      />
     </section>
   );
 }
 
-const Book = () => {
+const Book = (props) => {
+  const { img, title, author, children } = props;
   return (
-    <article className='book'>
-      <Image />
-      <Title />
-      <Author />
+    <article className="book">
+      <img src={img} alt="" />
+      <h1>{title}</h1>
+      <h4>{author}</h4>
+      {children}
     </article>
   );
 };
 
-const Image = () => (
-  <img
-    src="https://images-na.ssl-images-amazon.com/images/I/81eB%2B7%2BCkUL._AC_UL200_SR200,200_.jpg"
-    alt=""
-  />
-);
-
-const Title = () => <h1>I Love You to the Moon and Back</h1>;
-const Author = () => <h4>Amelia Hepworth</h4>;
 ReactDOM.render(<BookList />, document.getElementById("root"));

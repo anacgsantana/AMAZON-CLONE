18680997f1f7bfe86e0bb87a1a888f5699e15173
anacgsantana <anacgsantana@gmail.com>
2021-09-20 16:53:51 -0300

    update

---
+++ src/books.js
@@ -0,0 +1,22 @@
+export const books = [
+  {
+    id: 1,
+    img: "https://images-na.ssl-images-amazon.com/images/I/81eB%2B7%2BCkUL._AC_UL200_SR200,200_.jpg",
+    title: "I Love You to the moon and Back",
+    author: "Amelia Hepworth",
+  },
+
+  {
+    id: 2,
+    img: "https://m.media-amazon.com/images/I/71e38cCzk2L._AC_UY218_.jpg",
+    title: "More than a Princess",
+    author: "Delanda Coleman",
+  },
+  {
+    id: 3,
+    img: "https://m.media-amazon.com/images/I/713cuUqsE-L._AC_UY218_.jpg",
+    title:
+      "Madam C.J. Walker Builds a Business (A Good Night Stories for Rebel Girls Chapter Book)",
+    author: "Rebel Girls and Salini Perera",
+  },
+];
--- src/index.js
+++ src/index.js
@@ -4,45 +4,17 @@ import ReactDOM from "react-dom";
 //CSS
 
 import "./index.css";
-const books = [
-  {
-    img: "https://images-na.ssl-images-amazon.com/images/I/81eB%2B7%2BCkUL._AC_UL200_SR200,200_.jpg",
-    title: "I Love You to the moon and Back",
-    author: "Amelia Hepworth",
-  },
-
-  {
-    img: "https://m.media-amazon.com/images/I/71e38cCzk2L._AC_UY218_.jpg",
-    title: "More than a Princess",
-    author: "Delanda Coleman",
-  },
-  {
-    img: "https://m.media-amazon.com/images/I/713cuUqsE-L._AC_UY218_.jpg",
-    title:
-      "Madam C.J. Walker Builds a Business (A Good Night Stories for Rebel Girls Chapter Book)",
-    author: "Rebel Girls and Salini Perera",
-  },
-];
+import { books } from "./books";
+import Book from "./Book";
 
 function BookList() {
   return (
     <section className="booklist">
-      {books.map((book) => {
-        return <Book book={book}></Book>;
+      {books.map((book, index) => {
+        return <Book key={book.id} {...book}></Book>;
       })}
     </section>
   );
 }
-
-const Book = (props) => {
-  const { img, title, author } = props.book;
-  return (
-    <article className="book">
-      <img src={img} alt="" />
-      <h1>{title}</h1>
-      <h4>{author}</h4>
-    </article>
-  );
-};
 
 ReactDOM.render(<BookList />, document.getElementById("root"));
